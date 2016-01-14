90846c7e4473b6b06ce37b6cc612eda5c7050a40
Jean Wisser <jean.wisser@ysance.com>
2016-01-14 12:41:51 +0100

    modif

--- test.php
+++ test.php
@@ -1,4 +1,5 @@
 <?php
 $jobs = file_get_contents('http://109.232.232.41:19888/ws/v1/history/mapreduce/jobs');
-echo $jobs;
+$array = json_decode($jobs, true);
+var_dump($array["jobs"]["job"][0]);
 ?>
