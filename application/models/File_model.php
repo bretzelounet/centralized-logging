4eb58ed4a3e2fcbb3c8ec25a31c77000e942027f
bretzelounet <janowiss@hotmail.com>
2016-01-15 19:47:01 +0100

    add files

--- application/models/File_model.php
+++ application/models/File_model.php
@@ -17,9 +17,9 @@ class File_model extends CI_Model {
     
         public function get_applications()
         {
-           $jobs = json_decode(file_get_contents('http://109.232.232.41:19888/ws/v1/history/mapreduce/jobs'),true);
-           var_dump(end($jobs["jobs"]["job"]));
-            
+            $tmp = json_decode(file_get_contents('http://109.232.232.41:19888/ws/v1/history/mapreduce/jobs'),true);
+            $apps = array_reverse($tmp["jobs"]["job"]);
+            return $apps;
         }
 
 }
--- application/views/yarn.php
+++ application/views/yarn.php
@@ -12,14 +12,47 @@
 
   <div class="section">
     <div class="container">
-      <h4 class="orange-text">Yarn logs</h4>
+      <h4 class="orange-text title">Yarn applications</h4>
+        <table class="striped table-apps">
+        <thead>
+          <tr>
+              <th data-field="id">Job ID</th>
+              <th data-field="user">User</th>
+              <th data-field="name">Name</th>
+              <th data-field="queue">Queue</th>
+              <th data-field="started">Started</th>
+              <th data-field="finished">Finished</th>
+              <th data-field="duration">Duration</th>
+              <th data-field="status">Status</th>
+          </tr>
+        </thead>
+
+        <tbody>
+        <?php foreach($apps as $a){
+    
+        /* Time calculation */
+        $start = new DateTime('@'.substr($a["startTime"],0,-3));	
+        $start->setTimezone(new DateTimeZone('Europe/Paris'));
+        $end = new DateTime('@'.substr($a["finishTime"],0,-3));
+        $end->setTimezone(new DateTimeZone('Europe/Paris'));
+        $duration = $start->diff($end);
+        ?>
+          <tr>
+            <td><?php echo substr($a["id"],4); ?></td>
+            <td><?php echo $a["user"]; ?></td>
+            <td><?php echo $a["name"]; ?></td>
+            <td><?php echo $a["queue"]; ?></td>
+            <td><?php echo $start->format('D H:i:s'); ?></td>
+            <td><?php echo $end->format('D H:i:s'); ?></td>
+            <td><?php echo $duration->format('%H:%M:%S'); ?></td>
+            <td><span class="label green"><?php echo $a["state"]; ?></span></td>
+          </tr>
+        <?php } ?>
+        </tbody
+      </table>
     </div>
   </div>
 
-
-  <div class="container">
-
-  </div>
 
   <footer class="page-footer orange">
   </footer>
