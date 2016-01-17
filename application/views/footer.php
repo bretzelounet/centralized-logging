e1640c0f6559f0006a2f68106498436a1541c782
bretzelounet <janowiss@hotmail.com>
2016-01-17 04:30:45 +0100

    add github logo

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,5 +1,5 @@
 <footer class="page-footer orange">
-    <span style="color:white;padding : 0 5%;"class="center-align">© 2016 Jean Wisser</span>
+    <div style="text-align:right;"><a href="https://github.com/jeanwisser/centralized_logging" target="_blank"><img src="<?php echo base_url("assets/img/git.png"); ?>"></a></div>
 </footer>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url("assets/js/materialize.js"); ?>"></script>
