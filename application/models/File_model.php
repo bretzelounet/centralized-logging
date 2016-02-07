59c0240b2b0b66d5561c6a482fb1818e96edd008
bretzelounet <janowiss@hotmail.com>
2016-02-08 00:44:03 +0100

    add setting controller and view

--- application/models/File_model.php
+++ application/models/File_model.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class File {
 
-        public function get_file_content($user, $node_id)
+        public function get_params($user, $node_id)
         {
             $content = shell_exec('C:\"Program Files (x86)"\Gow\bin\cat.exe D:\test\logs\\'.$user.'\logs\\'.$node_id);
             return $content;
